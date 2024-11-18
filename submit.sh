561d1c31af2d945c8792630b07028e7f6032cf91
Dara Samii <darasamii@gmail.com>
2024-11-17 22:47:43 -0500

    fxed sbatch

--- submit.sh
+++ submit.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
-
+#SBATCH --job-name=CR25
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --ntasks=9
+#SBATCH --cpus-per-task=1
+#SBATCH --time=12:0:0    
+#SBATCH --mail-user=darasamii@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --account=def-tembelym
+#SBATCH --output=submit.out
 module load openfoam/11
 
 
