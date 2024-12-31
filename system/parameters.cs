dfd8e6ca8e5c094633e52a8af93c03bdc783714e
Dara Samii <darasamii@gmail.com>
2024-12-30 20:27:32 -0500

    this one works perfectly with rocke version 1

--- system/parameters.cs
+++ system/parameters.cs
@@ -1,30 +1,30 @@
 
 // **Define Parameters for Mesh Dimensions**
-xMin -7;
-xMax  10;
+xMin -6;
+xMax  8;
 
-yMin -6;
-yMax  6;
+yMin -5;
+yMax  5;
 
-zMin -6;
-zMax  6;
+zMin -5;
+zMax  5;
 
 // **Define Number of Cells**
-cellSize 0.1;  // Adjust cell size as needed
+cellSize 0.2;  // Adjust cell size as needed
 
 
 Ux 600;
-Uy 148.16;
-Uz 0;
+Uy 0.0;
+Uz 0.0;
 P 100000;
 T 300;
 
 maxCo 0.5;
 deltaT 2e-07;
-maxDeltaT 2e-07;
+maxDeltaT 5e-06;
 
 
 firstLayerThickness 0.1;
 minThickness    1e-5;
 
-refinementRegionsLevels ((0.3 4) (0.6 3) (1.2 2) (2.4 1));
+refinementRegionsLevels ((0.3 3) (0.6 2) (1.2 1));// (2.4 1));
