df16069c705d611bf9c7de884bb9f7e4ac4c0683
Dara Samii <darasamii@gmail.com>
2025-01-09 17:23:59 -0500

    changed to version 4 rocket

---
+++ original/dynamicCode/_8f7c6b6770a7402147ea9f917d3b93ec494eb0cb/codeStreamTemplate.C
@@ -0,0 +1,75 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) YEAR OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "fieldTypes.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "read.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    void codeStream_8f7c6b6770a7402147ea9f917d3b93ec494eb0cb
+    (
+        Ostream& os,
+        const dictionary& dict
+    )
+    {
+//{{{ begin code
+        #line 1 "/home/dara/Professional/projects/spaceConcordia/CR25/original/system/controlDict/functions/forceCoeffs"
+os << (sqrt(pow(dict.lookupScoped<label>("Ux", true, false) * 1.0,2) + pow(dict.lookupScoped<scalar>("Uy", true, false) * 1.0,2) + pow(dict.lookupScoped<scalar>("Uz", true, false) * 1.0,2)));
+//}}} end code
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
