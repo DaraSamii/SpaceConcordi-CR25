e5f9dedcb923c706e702e0cf8086746a6dea429d
Dara Samii <darasamii@gmail.com>
2025-05-11 01:48:05 -0400

    updated stl and cleaned parameters

--- original/parameters.cs
+++ original/parameters.cs
@@ -1,13 +1,13 @@
 
 // **Define Parameters for Mesh Dimensions**
 xMin -5;
-xMax 20;
+xMax 25;
 
-yMin -10;
-yMax  10;
+yMin -6;
+yMax  6;
 
-zMin -10;
-zMax  10;
+zMin -6;
+zMax  6;
 
 // **Define Number of Cells**
 maxCellSize 0.5;
@@ -18,15 +18,12 @@ P 69681;
 T 268;
 
 
-Ma      0.8;
+Ma      1.2;
 T       288.15;
 gamma   1.4;
 R       287.05;
 
 
-AoA 10;
+AoA 3;
 
-WriteInterval 10;
-
-
-refinementRegionsLevels ((0.1 3) (0.2 2) (0.4 1));
+WriteInterval 50;
